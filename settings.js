967cd7ea546dc5b4ee9aa449dda5be2b06a80533
Jim <jimmyjames85@users.noreply.github.com>
2016-08-17 16:21:33 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-exports.url = 'http://dc-opt-in.herokuapp.com';
+exports.url = 'http://doubleoptin.herokuapp.com';
 exports.senderEmail = "jtappe@jamestappe.com";
 exports.senderName = "Testing Devins double opt-in plugin";
 exports.listID = 511780;
